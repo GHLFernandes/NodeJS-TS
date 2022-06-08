e6a43ccc7a99bc51d9baf3c4d88eb56ca8720ccf
Gabriel Fernandes <fernandes.gh@outlook.com>
2022-06-08 19:29:23 -0300

    initiating typescript project

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import * as querystring from 'query-string';
+import * as url from 'url';
+import * as fs from 'fs';
+
+const PORT = process.env.PORT || 3000;
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+
+
+
+});
