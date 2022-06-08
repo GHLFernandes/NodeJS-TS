7391eb2e9675e78cfbea967443344e06579cc966
Gabriel Fernandes <fernandes.gh@outlook.com>
2022-06-08 19:29:23 -0300

    initiating typescript project

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const PORT = process.env.PORT || 3000;
+const server = (0, http_1.createServer)((req, res) => {
+});
